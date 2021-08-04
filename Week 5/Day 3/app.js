4437396ec71a178421e9602305db67e269cc281a
Kai Jenkins <86726403+KMJENK17@users.noreply.github.com>
2021-08-04 16:50:06 -0400

    adding day 3 of week 5

---
+++ Week 5/Day 3/app.js
@@ -0,0 +1,36 @@
+const showWeatherButton = document.getElementById('showWeatherButton')
+const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=atlanta&appid=bb611625250c7bcc999039fbf9a4a115&units=imperial`
+const usersUL = document.getElementById('usersUL')
+const cityName = document.getElementById('cityName')
+
+
+function weatherInfo(weather) {
+    fetch(weatherUrl)
+        .then(response => {
+            return response.json()
+        }).then(result => {
+            console.log(result)
+            displayWeather(result)
+        }).catch(error => {
+            console.log(error)
+        })
+}
+
+function displayWeather(data) {
+    const weatherData = data.main
+    const weatherInfoLabel =
+        ` <h1>${data.name}</h1> 
+    <label>Min Temperature: ${weatherData.temp_min}</label>
+    <label>Max Temperature: ${weatherData.temp_max}</label>
+    <label>Pressure: </b> ${weatherData.pressure}</label> `
+    usersUL.innerHTML = weatherInfoLabel
+}
+
+weatherInfo(function(result) {
+    displayWeather(result)
+
+})
+showWeatherButton.addEventListener('click', function() {
+    const cityNameInfo = cityName.value
+    displayWeather(cityNameInfo)
+})
